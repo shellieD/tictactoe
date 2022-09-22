b814cd0b23bbe32ff5f8478262983b3d57a231fd
shellieD <shellie.downie13@gmail.com>
2022-09-22 15:00:57 +0000

    Final commit.

--- js/game.js
+++
@@ -1,6 +0,0 @@
-export default class Game {
-    constructor() {
-        this.turn = "X"
-        this.board = new Array(9).fill(null)
-    }
-}
--- js/script.js
+++
@@ -1,6 +0,0 @@
-import Game from "./game";
-
-let game = new Game();
-
-console.log(game.turn);
-console.log(game.board);
